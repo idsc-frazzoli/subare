2ada97bd390696e666ca5e64b21211657e04c364
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-06-07 10:50:03 +0200

    travis ci

--- src/test/java/ch/ethz/idsc/subare/util/PoissonDistributionTest.java
+++ src/test/java/ch/ethz/idsc/subare/util/PoissonDistributionTest.java
@@ -2,12 +2,19 @@
 package ch.ethz.idsc.subare.util;
 
 import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.red.Total;
 import junit.framework.TestCase;
 
 public class PoissonDistributionTest extends TestCase {
-  public static void main(String[] args) {
+  public void testSimple() {
     PoissonDistribution poissonDistribution = PoissonDistribution.of(RealScalar.of(2));
-    // Scalar expected = poissonDistribution.values.dot(Range.of(0, poissonDistribution.values.length())).Get();
-    // System.out.println(expected);
+    Tensor prob = Tensors.vector(i -> poissonDistribution.apply(i), 16);
+    Scalar scalar = Total.of(prob).Get();
+    assertTrue(Scalars.lessThan(RealScalar.of(.9999), scalar));
+    assertTrue(Scalars.lessThan(scalar, RealScalar.ONE));
   }
 }
