03945312520e8cd100e27cbee420b1013ccd853b
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-07-28 14:38:33 +0200

    colorscheme classic from resourcedata

--- src/main/java/ch/ethz/idsc/subare/util/Colorscheme.java
+++ src/main/java/ch/ethz/idsc/subare/util/Colorscheme.java
@@ -2,18 +2,29 @@
 package ch.ethz.idsc.subare.util;
 
 import java.io.File;
+import java.io.IOException;
 
 import ch.ethz.idsc.tensor.io.Import;
+import ch.ethz.idsc.tensor.io.ResourceData;
 import ch.ethz.idsc.tensor.opt.Interpolation;
 import ch.ethz.idsc.tensor.opt.LinearInterpolation;
 
 public enum Colorscheme {
   ;
-  private static final Interpolation CLASSIC = of("classic");
-  // LinearInterpolation.of(ResourceData.of("/colorscheme/classic.csv"));
-  private static final Interpolation PARULA = of("parula");
+  private static final Interpolation CLASSIC = loadClassic();
 
-  private static Interpolation of(String string) {
+  private static Interpolation loadClassic() {
+    try {
+      return LinearInterpolation.of(ResourceData.of("/colorscheme/classic.csv"));
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    }
+    return null;
+  }
+
+  private static final Interpolation PARULA = load("parula");
+
+  private static Interpolation load(String string) {
     try {
       return LinearInterpolation.of( //
           Import.of(new File("".getClass().getResource("/util/colorscheme/" + string + ".csv").getPath())));
--- src/test/java/ch/ethz/idsc/subare/util/ColorschemeTest.java
+++ src/test/java/ch/ethz/idsc/subare/util/ColorschemeTest.java
@@ -10,16 +10,4 @@ public class ColorschemeTest extends TestCase {
     Interpolation interpolation = Colorscheme.classic();
     assertTrue(interpolation.get(Tensors.vector(200, 1)).isScalar());
   }
-
-  public void testTensor027() throws Exception {
-    // jar:file:/home/datahaki/.m2/repository/ch/ethz/idsc/tensor/0.2.7/tensor-0.2.7.jar!/colorscheme/classic.csv
-    // URL url = ResourceData.url("/colorscheme/classic.csv");
-    // System.out.println(url);
-    // // jar:file:/home/datahaki/.m2/repository/ch/ethz/idsc/tensor/0.2.7/tensor-0.2.7.jar!/colorscheme/classic.csv
-    // URI uri = ResourceData.uri("/colorscheme/classic.csv");
-    // System.out.println(uri);
-    // System.out.println(uri.getPath());
-    // Tensor tensor = ResourceData.of("/colorscheme/classic.csv");
-    // System.out.println(tensor);
-  }
 }
