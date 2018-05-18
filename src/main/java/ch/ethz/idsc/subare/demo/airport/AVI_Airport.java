81ca86fb5353b463a53346f3f435ee98865ee1aa
fluric <fluric@student.ethz.ch>
2018-05-18 20:39:07 +0200

    correct action state

--- src/main/java/ch/ethz/idsc/subare/demo/airport/AVI_Airport.java
+++ src/main/java/ch/ethz/idsc/subare/demo/airport/AVI_Airport.java
@@ -7,20 +7,15 @@ import ch.ethz.idsc.subare.core.util.DiscreteUtils;
 import ch.ethz.idsc.subare.core.util.GreedyPolicy;
 import ch.ethz.idsc.subare.core.util.Policies;
 import ch.ethz.idsc.tensor.DecimalScalar;
-import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.Tensor;
 
 enum AVI_Airport {
   ;
   public static void main(String[] args) {
     Airport airport = new Airport();
-
     DiscreteQsa qsa = ActionValueIterations.solve(airport, DecimalScalar.of(.0001));
-    
     DiscreteUtils.print(qsa);
-    qsa.value(Tensors.vector(0,5,0), Tensors.vector(1,-1,1));
     Policy policy = GreedyPolicy.bestEquiprobable(airport, qsa);
-    
     Policies.print(policy, airport.states());
-    
   }
 }
--- src/main/java/ch/ethz/idsc/subare/demo/airport/Airport.java
+++ src/main/java/ch/ethz/idsc/subare/demo/airport/Airport.java
@@ -1,54 +1,63 @@
 // code fluric
 package ch.ethz.idsc.subare.demo.airport;
 
+import java.util.Random;
+
 import ch.ethz.idsc.subare.core.MonteCarloInterface;
 import ch.ethz.idsc.subare.core.StandardModel;
+import ch.ethz.idsc.subare.util.GlobalAssert;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.pdf.BernoulliDistribution;
+import ch.ethz.idsc.tensor.pdf.Distribution;
+import ch.ethz.idsc.tensor.red.Max;
+import ch.ethz.idsc.tensor.red.Total;
 
-/** Example 4.3 p.90: Gambler's problem
- * an action defines the amount of coins to bet
- * the action has to be non-zero unless the capital == 0
- * or the terminal cash has been reached
- * 
- * [no further references are provided in the book] */
+/** A two node problem with an airport and a center. Passengers arrive at the airport and can be driven to
+ * the center by taxis. The taxis don't know in advance if there are passengers to pick up when they move
+ * from the airport to the center. Driving without passenger to the other node costs 10CHF. Driving from
+ * the airport to the center with a customers gives 30CHF reward instead. Parking at the airport for one
+ * time step at the airport costs 5CHF. */
 class Airport implements StandardModel, MonteCarloInterface {
   private static final int LASTT = 3;
   private static final int VEHICLES = 5;
+  private static final Scalar REBALANCE_COST = RealScalar.of(10);
+  private static final Scalar AIRPORT_WAIT_COST = RealScalar.of(5);
+  private static final Scalar CUSTOMER_REWARD = RealScalar.of(30);
   private final Tensor states;
+  private Random random = new Random();
+  private static final Tensor CUSTOMER_PROB = Tensors.vectorDouble(0.4, 0.2, 0.1, 0.3); // i.e. CUSTOMER_PROB.Get(0) is the probability that no customer is
+                                                                                        // waiting
 
-  /** @param max stake
-   * @param P_win probabilty of winning a coin toss */
   public Airport() {
     states = Tensors.empty();
-    states.append(Tensors.vector(0, 5, 0));
+    states.append(Tensors.vector(0, 5, 0)); // start at time 0 with 5 taxis in the city and 0 in the airport
     for (int t = 1; t <= LASTT; t++) {
       for (int v = 0; v <= VEHICLES; v++) {
         states.append(Tensors.vector(t, v, VEHICLES - v));
       }
     }
+    GlobalAssert.that(Total.of(CUSTOMER_PROB).equals(RealScalar.of(1.0)));
+    GlobalAssert.that(Total.of(states.get(0)).equals(RealScalar.of(VEHICLES)));
   }
 
   @Override
-  public Tensor states() { // done
+  public Tensor states() {
     return states;
   }
 
   /** @return possible stakes */
   @Override
-  public Tensor actions(Tensor state) { // done
+  public Tensor actions(Tensor state) {
     if (isTerminal(state))
-      return Tensors.fromString("{{0}}");
-    // return Tensors.of(RealScalar.ZERO);
-    // here we deviate from the book and the code by STZ:
-    // we require that the bet=action is non-zero,
-    // if the state is non-terminal, 0 < cash < 100.
-    // otherwise the player can stall (the iteration) forever.
+      return Tensors.of(RealScalar.ZERO);
     Tensor actions = Tensors.empty();
-    for (int i = -state.Get(2).number().intValue(); i <= state.Get(1).number().intValue(); i++) {
-      actions.append(Tensors.vector(1, - i, i));
+    for (int i = 0; i <= state.Get(1).number().intValue(); i++) {
+      for (int j = 0; j <= state.Get(2).number().intValue(); j++) {
+        actions.append(Tensors.vector(i, j));
+      }
     }
     return actions;
   }
@@ -60,11 +69,13 @@ class Airport implements StandardModel, MonteCarloInterface {
 
   /**************************************************/
   @Override
-  public Tensor move(Tensor state, Tensor action) { // done
+  public Tensor move(Tensor state, Tensor action) {
     if (isTerminal(state)) {
+      GlobalAssert.that(action.equals(RealScalar.ZERO));
       return state;
     }
-    return state.add(action);
+    return Tensors.vector(state.Get(0).add(RealScalar.ONE).number(), state.Get(1).subtract(action.Get(0)).add(action.Get(1)).number(),
+        state.Get(2).subtract(action.Get(1)).add(action.Get(0)).number());
   }
 
   @Override
@@ -73,12 +84,12 @@ class Airport implements StandardModel, MonteCarloInterface {
   }
 
   /**************************************************/
-  @Override // from MonteCarloInterface // done
+  @Override // from MonteCarloInterface
   public Tensor startStates() {
     return states.extract(0, 1);
   }
 
-  @Override // from TerminalInterface, done
+  @Override // from TerminalInterface
   public boolean isTerminal(Tensor state) {
     return state.get(0).equals(RealScalar.of(LASTT));
   }
@@ -88,24 +99,24 @@ class Airport implements StandardModel, MonteCarloInterface {
   public Scalar expectedReward(Tensor state, Tensor action) {
     if (isTerminal(state))
       return RealScalar.ZERO;
+
     if (state.Get(0).equals(RealScalar.of(2)) && state.Get(2).equals(RealScalar.of(3)))
       return RealScalar.ONE;
     return RealScalar.ZERO; // TODO
   }
 
   @Override
-  public Tensor transitions(Tensor state, Tensor action) { // done
-    if (isTerminal(state))
-      return Tensors.of(state); // next \in {state}
-    return Tensors.of( //
-        state.add(action));
+  public Tensor transitions(Tensor state, Tensor action) {
+    return Tensors.of(move(state, action));
   }
 
   @Override
   public Scalar transitionProbability(Tensor state, Tensor action, Tensor next) {
-    if (isTerminal(state))
+    if (isTerminal(state)) {
+      GlobalAssert.that(move(state, action).equals(next));
       return RealScalar.ONE;
-    if (state.add(action).equals(next))
+    }
+    if (move(state, action).equals(next))
       return RealScalar.ONE;
     return RealScalar.ZERO;
   }
