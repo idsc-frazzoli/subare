3768bb185116f4dd21e66648ebe6bdc04d99b2e8
fluric <fluric@student.ethz.ch>
2018-08-10 10:37:17 +0200

    parameter tuning for cascadeModel in amodidsc

--- src/main/java/ch/ethz/idsc/subare/analysis/SarsaMonteCarloTrial.java
+++ src/main/java/ch/ethz/idsc/subare/analysis/SarsaMonteCarloTrial.java
@@ -23,7 +23,7 @@ import ch.ethz.idsc.tensor.Scalar;
 public class SarsaMonteCarloTrial implements MonteCarloTrial {
   private static final Scalar ALPHA = RealScalar.of(0.05);
   private static final Scalar EPSILON = RealScalar.of(0.1);
-  private static final int DIGEST_DEPTH = 1; // 0 is equal to the MonteCarlo approach
+  private static final int DIGEST_DEPTH = 5; // 0 is equal to the MonteCarlo approach, 5 is a good number for CascadeModel in amodidsc
   // ---
   private final MonteCarloInterface monteCarloInterface;
   private final Sarsa sarsa;
